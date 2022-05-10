f43f84f716b8ebe7481c317e17d2814c06390227
jarley Saldarriaga <auxsistemas5@hmfs.gov.co>
2022-05-10 07:46:54 -0500

    tabla de calificacion para prehospitalizacion

--- Especialistas/partials/_calificar.php
+++ Especialistas/partials/_calificar.php
@@ -12,8 +12,19 @@
     $servicio = $datoEspe[2];
     $fechaActual = date('m');
 
-    $sqlEstudiantesPorEspecialista = "SELECT registro.id,servicios.services_name,registro.admission_date,usuarios.id,usuarios.username,usuarios.document,usuarios.phone,usuarios.position,programas.programs_name,usuarios.email, usuarios.photo,registro.finish_date
-    FROM registro INNER JOIN programas ON programas.id = registro.id_program INNER JOIN servicios ON servicios.id = registro.id_service INNER JOIN usuarios ON usuarios.id = registro.id_user WHERE  usuarios.position = 'INTERNO' AND registro.anulated = 0  AND servicios.services_name = '$servicio' AND registro.finish_date BETWEEN '2022-03-01' AND '2022-03-31'  ORDER BY registro.admission_date";
+
+    if($servicio == 'PREHOSPITALIZACIÓN'){
+        $sqlEstudiantesPorEspecialista = "SELECT registro.id,servicios.services_name,registro.admission_date,usuarios.id,usuarios.username,usuarios.document,usuarios.phone,usuarios.position,programas.programs_name,usuarios.email, usuarios.photo,registro.finish_date
+        FROM registro INNER JOIN programas ON programas.id = registro.id_program INNER JOIN servicios ON servicios.id = registro.id_service INNER JOIN usuarios ON usuarios.id = registro.id_user WHERE  usuarios.position = 'INTERNO' AND registro.anulated = 0 AND servicios.services_name = 'MEDICINA INTERNA' AND registro.finish_date BETWEEN '2022-03-01' AND '2022-03-31'   ORDER BY registro.admission_date";
+
+        $sqlEstudiantesPorEspecialista1 = "SELECT registro.id,servicios.services_name,registro.admission_date,usuarios.id,usuarios.username,usuarios.document,usuarios.phone,usuarios.position,programas.programs_name,usuarios.email, usuarios.photo,registro.finish_date
+        FROM registro INNER JOIN programas ON programas.id = registro.id_program INNER JOIN servicios ON servicios.id = registro.id_service INNER JOIN usuarios ON usuarios.id = registro.id_user WHERE  usuarios.position = 'INTERNO' AND registro.anulated = 0 AND servicios.services_name = 'CIRUGÍA' AND registro.finish_date BETWEEN '2022-03-01' AND '2022-03-31'   ORDER BY registro.admission_date";
+    }else{
+
+        $sqlEstudiantesPorEspecialista = "SELECT registro.id,servicios.services_name,registro.admission_date,usuarios.id,usuarios.username,usuarios.document,usuarios.phone,usuarios.position,programas.programs_name,usuarios.email, usuarios.photo,registro.finish_date
+        FROM registro INNER JOIN programas ON programas.id = registro.id_program INNER JOIN servicios ON servicios.id = registro.id_service INNER JOIN usuarios ON usuarios.id = registro.id_user WHERE  usuarios.position = 'INTERNO' AND registro.anulated = 0  AND servicios.services_name = '$servicio' AND registro.finish_date BETWEEN '2022-03-01' AND '2022-03-31'  ORDER BY registro.admission_date";
+    }
+
     /*
     //valida las fechas en rango de dos meses para calificar los usuarios
     if($fechaActual == '01'){
@@ -60,120 +71,15 @@
         FROM registro INNER JOIN programas ON programas.id = registro.id_program INNER JOIN servicios ON servicios.id = registro.id_service INNER JOIN usuarios ON usuarios.id = registro.id_user WHERE usuarios.state = 1 AND usuarios.position = 'INTERNO' AND servicios.services_name = '$servicio' AND registro.finish_date BETWEEN '2022-11-01' AND '2022-12-31'  ORDER BY registro.admission_date";
     }
     */
-    $sqlConsultaEstudiante = mysqli_query($conexion,$sqlEstudiantesPorEspecialista);
+    
 
+
+    if($servicio == 'PREHOSPITALIZACIÓN'){
+        $sqlConsultaEstudiante = mysqli_query($conexion,$sqlEstudiantesPorEspecialista);
+        $sqlConsultaEstudiante1 = mysqli_query($conexion,$sqlEstudiantesPorEspecialista1);
+        require '_calificar_prehospitalizacion.php';
+    }else{
+        $sqlConsultaEstudiante = mysqli_query($conexion,$sqlEstudiantesPorEspecialista);
+        require '_calificar_student.php';
+    }
 ?>
-<div class="col-md-12">
-    <div class="card">
-        <div class="card-header">
-            <h4>Estudiantes a Calificar</h4>
-            <span class="badge badge-danger">RECUERDE...SI NO CONOCE EL ESTUDIANTE POR FAVOR DEJARLO SIN VALORES Y SOLO PRESIONA CALIFICAR </span>
-        </div>
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table" id="tabla_resultados">
-                    <thead>
-                        <tr>
-                            <th>NOMBRE</th>
-                            <th>FOTO</th>
-                            <th>MES A CALIFICAR</th>
-                            <th>CONOCIMIENTO</th>
-                            <th>ACTITUD</th>
-                            <th>SEMINARIO</th>
-                            <th>OBSERVACIONES</th>
-                            <th>ACCION</th>
-                            
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                                if($sqlConsultaEstudiante){
-                                    while($datosEstudiantes = mysqli_fetch_array($sqlConsultaEstudiante)){
-                                            $nombre = $_SESSION['name'];
-                                            $estudiante = $datosEstudiantes['username'];
-                                            //verifica si ya se le califico al estudiante
-                                            $sqlRevisarResultados = "SELECT id FROM calificacion_docente WHERE  (student = '$estudiante') AND (docente = '$nombre')";
-
-                                            $sqlBuscarCalificacion = mysqli_query($conexion,$sqlRevisarResultados);
-
-                                            $verificar = mysqli_fetch_array($sqlBuscarCalificacion);
-
-                                            if($verificar > 0){
-                                                
-                                            }else{
-                                                
-                                        ?>
-                                            <tr>
-                                                <form action="../logica/add_calification_specialist.php?student=<?php echo $datosEstudiantes['username'] ?>&nombre=<?php echo $_SESSION['name'];  ?>" method="POST" id="formulario">
-
-
-                                                    <td><?php echo $datosEstudiantes['username'];  ?></td>
-
-                                                    <td><img src="<?php echo $datosEstudiantes['photo'] ?>" width='120px' height='120px'></td>
-
-                                                    <td><?php
-                                                        $finalizacion = $datosEstudiantes['finish_date'];
-                                                        $dates = explode('-',$finalizacion);
-                                                        if($dates[1] == '01'){
-                                                            echo "ENERO";
-                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
-                                                        }else if($dates[1] == '02'){
-                                                            echo "FEBRERO";
-                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
-                                                        }else if($dates[1] == '03'){
-                                                            echo "MARZO";
-                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
-                                                        }else if($dates[1] == '04'){
-                                                            echo "ABRIL";
-                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
-                                                        }else if($dates[1] == '05'){
-                                                            echo "MAYO";
-                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
-                                                        }else if($dates[1] == '06'){
-                                                            echo "JUNIO";
-                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
-                                                        }else if($dates[1] == '07'){
-                                                            echo "JULIO";
-                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
-                                                        }else if($dates[1] == '08'){
-                                                            echo "AGOSTO";
-                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
-                                                        }else if($dates[1] == '09'){
-                                                            echo "SEPTIEMBRE";
-                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
-                                                        }else if($dates[1] == '10'){
-                                                            echo "OCTUBRE";
-                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
-                                                        }else if($dates[1] == '11'){
-                                                            echo "NOVIEMBRE";
-                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
-                                                        }else if($dates[1] == '12'){
-                                                            echo "DICIEMBRE";
-                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
-                                                        }
-                                                        ?>
-                                                    </td>
-                                                    
-                                                    <td><input id="conocimiento" min="0" max="5" type="number" step="any"  name="conocimiento" class="form-control"></td>
-
-                                                    <td><input id="actitud" min="0" max="5"  type="number" step="any"  name="actitud" class="form-control"></td>
-
-                                                    <td><input type="text" min="0" max="5"  name="seminario" class="form-control"></td>
-
-                                                    <td>
-                                                        <textarea class="form-control" name="observacion" id="" cols="10" rows="3"></textarea>
-                                                    </td>
-                                                    <td><button id="calificar" class="btn btn-dark btn-sm" type="submit">Calificar</button></td>
-                                                </form>
-                                            </tr>
-                                        <?php
-                                            }
-                                    }
-                                }
-                            ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-</div>
---
+++ Especialistas/partials/_calificar_prehospitalizacion.php
@@ -0,0 +1,229 @@
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header">
+            <h4>Estudiantes a Calificar</h4>
+            <span class="badge badge-danger">RECUERDE...SI NO CONOCE EL ESTUDIANTE POR FAVOR DEJARLO SIN VALORES Y SOLO PRESIONA CALIFICAR </span>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table" id="tabla_resultados">
+                    <thead>
+                        <tr>
+                            <th>NOMBRE</th>
+                            <th>FOTO</th>
+                            <th>MES A CALIFICAR</th>
+                            <th>CONOCIMIENTO</th>
+                            <th>ACTITUD</th>
+                            <th>SEMINARIO</th>
+                            <th>OBSERVACIONES</th>
+                            <th>ACCION</th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                                if($sqlConsultaEstudiante){
+                                    while($datosEstudiantes = mysqli_fetch_array($sqlConsultaEstudiante)){
+                                            $nombre = $_SESSION['name'];
+                                            $estudiante = $datosEstudiantes['username'];
+                                            //verifica si ya se le califico al estudiante
+                                            $sqlRevisarResultados = "SELECT id FROM calificacion_docente WHERE  (student = '$estudiante') AND (docente = '$nombre')";
+
+                                            $sqlBuscarCalificacion = mysqli_query($conexion,$sqlRevisarResultados);
+
+                                            $verificar = mysqli_fetch_array($sqlBuscarCalificacion);
+
+                                            if($verificar > 0){
+                                                
+                                            }else{
+                                                
+                                        ?>
+                                            <tr>
+                                                <form action="../logica/add_calification_specialist.php?student=<?php echo $datosEstudiantes['username'] ?>&nombre=<?php echo $_SESSION['name'];  ?>" method="POST" id="formulario">
+
+
+                                                    <td><?php echo $datosEstudiantes['username'];  ?></td>
+
+                                                    <td><img src="<?php echo $datosEstudiantes['photo'] ?>" width='120px' height='120px'></td>
+
+                                                    <td><?php
+                                                        $finalizacion = $datosEstudiantes['finish_date'];
+                                                        $dates = explode('-',$finalizacion);
+                                                        if($dates[1] == '01'){
+                                                            echo "ENERO";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '02'){
+                                                            echo "FEBRERO";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '03'){
+                                                            echo "MARZO";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '04'){
+                                                            echo "ABRIL";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '05'){
+                                                            echo "MAYO";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '06'){
+                                                            echo "JUNIO";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '07'){
+                                                            echo "JULIO";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '08'){
+                                                            echo "AGOSTO";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '09'){
+                                                            echo "SEPTIEMBRE";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '10'){
+                                                            echo "OCTUBRE";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '11'){
+                                                            echo "NOVIEMBRE";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '12'){
+                                                            echo "DICIEMBRE";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }
+                                                        ?>
+                                                    </td>
+                                                    
+                                                    <td><input id="conocimiento" min="0" max="5" type="number" step="any"  name="conocimiento" class="form-control"></td>
+
+                                                    <td><input id="actitud" min="0" max="5"  type="number" step="any"  name="actitud" class="form-control"></td>
+
+                                                    <td><input type="text" min="0" max="5"  name="seminario" class="form-control"></td>
+
+                                                    <td>
+                                                        <textarea class="form-control" name="observacion" id="" cols="10" rows="3"></textarea>
+                                                    </td>
+                                                    <td><button id="calificar" class="btn btn-dark btn-sm" type="submit">Calificar</button></td>
+                                                </form>
+                                            </tr>
+                                        <?php
+                                            }
+                                    }
+                                }
+                            ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header">
+            <h4>Estudiantes a Calificar</h4>
+            <span class="badge badge-danger">RECUERDE...SI NO CONOCE EL ESTUDIANTE POR FAVOR DEJARLO SIN VALORES Y SOLO PRESIONA CALIFICAR </span>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table" id="tabla_resultados">
+                    <thead>
+                        <tr>
+                            <th>NOMBRE</th>
+                            <th>FOTO</th>
+                            <th>MES A CALIFICAR</th>
+                            <th>CONOCIMIENTO</th>
+                            <th>ACTITUD</th>
+                            <th>SEMINARIO</th>
+                            <th>OBSERVACIONES</th>
+                            <th>ACCION</th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                                if($sqlConsultaEstudiante1){
+                                    while($datosEstudiantes = mysqli_fetch_array($sqlConsultaEstudiante1)){
+                                            $nombre = $_SESSION['name'];
+                                            $estudiante = $datosEstudiantes['username'];
+                                            //verifica si ya se le califico al estudiante
+                                            $sqlRevisarResultados = "SELECT id FROM calificacion_docente WHERE  (student = '$estudiante') AND (docente = '$nombre')";
+
+                                            $sqlBuscarCalificacion = mysqli_query($conexion,$sqlRevisarResultados);
+
+                                            $verificar = mysqli_fetch_array($sqlBuscarCalificacion);
+
+                                            if($verificar > 0){
+                                                
+                                            }else{
+                                                
+                                        ?>
+                                            <tr>
+                                                <form action="../logica/add_calification_specialist.php?student=<?php echo $datosEstudiantes['username'] ?>&nombre=<?php echo $_SESSION['name'];  ?>" method="POST" id="formulario">
+
+
+                                                    <td><?php echo $datosEstudiantes['username'];  ?></td>
+
+                                                    <td><img src="<?php echo $datosEstudiantes['photo'] ?>" width='120px' height='120px'></td>
+
+                                                    <td><?php
+                                                        $finalizacion = $datosEstudiantes['finish_date'];
+                                                        $dates = explode('-',$finalizacion);
+                                                        if($dates[1] == '01'){
+                                                            echo "ENERO";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '02'){
+                                                            echo "FEBRERO";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '03'){
+                                                            echo "MARZO";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '04'){
+                                                            echo "ABRIL";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '05'){
+                                                            echo "MAYO";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '06'){
+                                                            echo "JUNIO";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '07'){
+                                                            echo "JULIO";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '08'){
+                                                            echo "AGOSTO";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '09'){
+                                                            echo "SEPTIEMBRE";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '10'){
+                                                            echo "OCTUBRE";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '11'){
+                                                            echo "NOVIEMBRE";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }else if($dates[1] == '12'){
+                                                            echo "DICIEMBRE";
+                                                            echo "<input hidden name='mes_calificado' value='$finalizacion'>";
+                                                        }
+                                                        ?>
+                                                    </td>
+                                                    
+                                                    <td><input id="conocimiento" min="0" max="5" type="number" step="any"  name="conocimiento" class="form-control"></td>
+
+                                                    <td><input id="actitud" min="0" max="5"  type="number" step="any"  name="actitud" class="form-control"></td>
+
+                                                    <td><input type="text" min="0" max="5"  name="seminario" class="form-control"></td>
+
+                                                    <td>
+                                                        <textarea class="form-control" name="observacion" id="" cols="10" rows="3"></textarea>
+                                                    </td>
+                                                    <td><button id="calificar" class="btn btn-dark btn-sm" type="submit">Calificar</button></td>
+                                                </form>
+                                            </tr>
+                                        <?php
+                                            }
+                                    }
+                                }
+                            ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
